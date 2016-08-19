e30078bf1986d3473a08856e3494420e2e0112d8
pjhooker <lima.cityplanner@gmail.com>
2016-08-19 17:08:31 +0200

    Update cartodb_darkmatter.js

--- js/tile/cartodb_darkmatter.js
+++ js/tile/cartodb_darkmatter.js
@@ -1,5 +1,4 @@
-L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}.png', {
+L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
-  subdomains: 'abcd'
+  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
