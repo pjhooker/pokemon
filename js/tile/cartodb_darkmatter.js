aa9ecd1fb2d0404810a3fb5d4e34ef2df843fac7
pjhooker <lima.cityplanner@gmail.com>
2016-08-19 17:27:18 +0200

    Update cartodb_darkmatter.js

--- js/tile/cartodb_darkmatter.js
+++ js/tile/cartodb_darkmatter.js
@@ -1,4 +1,7 @@
-L.tileLayer('https://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: 'Stamen Design &copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 20,
+	ext: 'png'
 }).addTo(mymap);
